ef35fea14303ee7b1ccfe076b6ce8a90eb536163
afifa_o1 <ruhsonaholmirzaeva85@gmail.com>
2025-03-05 12:57:08 +0500

    added readme.md

--- index.php
+++ index.php
@@ -12,7 +12,6 @@
     <h1>QR skaner</h1>
     <form action="">
     <img src="https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=http%3A%2F%2Fwww.google.com%2F&choe=UTF-8" title="Link to Google.com" />
-
         <input type="text" placeholder ="enter link or text">
         <button type="submit">Send</button>
     </form>
---
+++ readme.md
@@ -0,0 +1,4 @@
+1) You need to install ngnix and connect it to php. 
+2) You need to create the html part.
+3) Need to content html to backend.
+4) Install qrkode library.
