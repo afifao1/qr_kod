4538d0124a05d2d5e3f733657f636b801e5f87b9
afifa_o1 <ruhsonaholmirzaeva85@gmail.com>
2025-03-05 12:40:49 +0500

    added html

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
 
-echo 'Hello from nginx';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>QR skaner</h1>
+    <form action="">
+    <img src="https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=http%3A%2F%2Fwww.google.com%2F&choe=UTF-8" title="Link to Google.com" />
+
+        <input type="text" placeholder ="enter link or text">
+        <button type="submit">Send</button>
+    </form>
+</body>
+</html>
